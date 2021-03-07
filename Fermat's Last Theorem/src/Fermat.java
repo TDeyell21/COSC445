1e114bb06e8edc72f4613329f2dbd4bf7f68dc7f
TDeyell21 <deyellt@duq.edu>
2021-03-07 17:10:18 -0500

    Commit 2: fixes some variable resolutions ; compiles, but runs incorrectly

--- Fermat's Last Theorem/src/Fermat.java
+++ Fermat's Last Theorem/src/Fermat.java
@@ -7,7 +7,7 @@ Programmers: Tim Deyell
 Email: deyellt@duq.edu 
 Course info: COSC 445W
 Submission date: 3/7/21
-Program summary: The attempted goal of this program is
+Program summary: The attempted goal of this program is to create a method for approximating pairs of numbers that closely satisfy Fermat's Final Theorem.
 External resources: 
 */
 
@@ -15,23 +15,26 @@ import java.util.Scanner;
 import java.lang.Math;
 
 public class Fermat{
-	static void fermatTest(){
+	public static void fermatTest(){
 		Scanner input= new Scanner(System.in); //creates a way for the user to interact with the program
+		
 		System.out.println("Fermat's Final Theorem states that there is no natural numbers that satisfy the equation x^n + y^n = z^n when n>2.");
 		System.out.println("Care to see if he's wrong?");
-		System.out.println("Pick an exponent between 2 and 12");
+		System.out.println("Pick an exponent between 2 and 12: ");
 		int n=input.nextInt();//creates a user generated exponent to test the theorem with
 		while (n>12 || n<1) {// ensures the exponent is in the valid range
 			System.out.println("Invalid exponent! Try again:");
 			n=input.nextInt();
 		}
-		System.out.println("Pick a search range value greater than 10 ");
+		System.out.println("Pick a search range value greater than 10: ");
 		int k=input.nextInt();//creates the search range value 
 		while (k<10) {//validates the search range criteria
 			System.out.println("Invalid value! Try again:");
 			k=input.nextInt();
 		}
 		int x=10; int y=10;// initializes variables to begin our search
+		double zFinal=0.0;//initializes variables that we call in the print
+		double missRatio=10;//initializes variable that's used in for loop; large value given to ensure it is replaced easily
 		
 		for (int i=10;i<k;i++) {
 			for(int j=10; j<k; j++) {
@@ -45,26 +48,25 @@ public class Fermat{
 				double diff1=pair-bound1;//creates difference to compare 
 				double diff2=bound2-pair;//creates difference to compare 
 				double miss=Math.min(diff1, diff2);
-				double missRatio=miss/pair;
+				missRatio=miss/pair;
 				if (missRatio<currentRatio) {
 					currentRatio=missRatio;
-					double zFinal=miss;//holds which z-value is being used 
+					zFinal=miss;//holds which z-value is being used 
 				}
 				y++;
-			} 	
+			} //end of inner for loop
 		x++;
 		System.out.println("The current approximation values are: ");
-		System.out.println("n= %d;x= %d ; y= %d ; z= %d",n, x,y,zFinal);
+		System.out.println("n= "+n+"; x= "+x+"; y= "+y+"; z= "+zFinal);
 		System.out.println("The current miss ratio is "+ missRatio);
 		System.out.println("Continue searching? ");
 		String nextLine=input.next();//creates an executbale to keep the program running and let the user analyze the current scenario 
-		while (nextLine.isEmpty()) {
+		while (nextLine.isEmpty()) 
 			{continue;}	
-	
-		}
-		}
+		}//end of outer for loop
+		input.close();
+	}
 	public static void main(String[] args) {
 		fermatTest();
-		}
 	}
 } 
