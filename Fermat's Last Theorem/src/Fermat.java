13e4b996f38950d9e2917dc9bd3d31af00458531
TDeyell21 <deyellt@duq.edu>
2021-03-07 19:19:10 -0500

    Commit 3: reorganization of the print statements and more effective indexing to print x,y,z

--- Fermat's Last Theorem/src/Fermat.java
+++ Fermat's Last Theorem/src/Fermat.java
@@ -1,14 +1,14 @@
 /** Opening Comment:
 Title: Fermat's Last Theorem
-File name: 
-External input files:
-External output files: 
-Programmers: Tim Deyell 
+File name: Fermat.java
+External input files: Fermat.java
+External output files: NA
+Programmers: Tim Deyell, Pavel Bogachenko, Kevin Chen, Ibrokhimkhuja Shokhujaev
 Email: deyellt@duq.edu 
 Course info: COSC 445W
 Submission date: 3/7/21
 Program summary: The attempted goal of this program is to create a method for approximating pairs of numbers that closely satisfy Fermat's Final Theorem.
-External resources: 
+External resources: NA
 */
 
 import java.util.Scanner;
@@ -36,33 +36,30 @@ public class Fermat{
 		double zFinal=0.0;//initializes variables that we call in the print
 		double missRatio=10;//initializes variable that's used in for loop; large value given to ensure it is replaced easily
 		
-		for (int i=10;i<k;i++) {
+		for (int i=10;i<k;i++) {//double for loops to check for all x,y combinations in our bounded range
 			for(int j=10; j<k; j++) {
-				double pair=(x^n)+(y^n); //creates the pair sum we are trying to approximate
+				double pair=(i^n)+(j^n); //creates the pair sum we are trying to approximate
 				double z=Math.pow(pair, 1/n);//approximates a z value
 				double zFloor=Math.floor(z);//creates a decimal that we'll raise to n 
 				double bound1=Math.pow(zFloor, n);//creates lower bound to test 
 				double bound2=Math.pow(zFloor+1,n);//creates upper bound to test 
-				double currentRatio=1.0; //intializes a variable used below 
+				double currentRatio=1.0; //initializes a variable used below 
 				
 				double diff1=pair-bound1;//creates difference to compare 
 				double diff2=bound2-pair;//creates difference to compare 
-				double miss=Math.min(diff1, diff2);
+				double miss=Math.min(diff1, diff2);//creates a minimum value to analyze which estimate is more accurate.
 				missRatio=miss/pair;
-				if (missRatio<currentRatio) {
+				if (missRatio<currentRatio) {//checks if the new miss is closer than our last closest miss
 					currentRatio=missRatio;
 					zFinal=miss;//holds which z-value is being used 
+					y=j;//saves the y-value for our new closer miss
+					x=i;//saves the x-value for our new closer miss
+					System.out.println("The current approximation values are: ");
+					System.out.println("n= "+n+"; x= "+x+"; y= "+y+"; z= "+zFinal);
+					System.out.println("The current miss ratio is "+ missRatio);
+					System.out.println('\n');
 				}
-				y++;
 			} //end of inner for loop
-		x++;
-		System.out.println("The current approximation values are: ");
-		System.out.println("n= "+n+"; x= "+x+"; y= "+y+"; z= "+zFinal);
-		System.out.println("The current miss ratio is "+ missRatio);
-		System.out.println("Continue searching? ");
-		String nextLine=input.next();//creates an executbale to keep the program running and let the user analyze the current scenario 
-		while (nextLine.isEmpty()) 
-			{continue;}	
 		}//end of outer for loop
 		input.close();
 	}
