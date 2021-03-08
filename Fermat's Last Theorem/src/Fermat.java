3e1b19cd8a5efbb15a3ce893847eb7a59ef46c1d
TDeyell21 <deyellt@duq.edu>
2021-03-07 19:29:33 -0500

    Commit 3?

--- Fermat's Last Theorem/src/Fermat.java
+++ Fermat's Last Theorem/src/Fermat.java
@@ -59,8 +59,8 @@ public class Fermat{
 					System.out.println("The current miss ratio is "+ missRatio);
 					System.out.println('\n');
 				}
-			} //end of inner for loop
-		}//end of outer for loop
+			} //end of inner for-loop
+		}//end of outer for-loop
 		input.close();
 	}
 	public static void main(String[] args) {
